22f9f58701f8699f089b6e582bdc90c6ccefe494
Ashes814 <1534661514@qq.com>
2023-06-06 21:12:36 +0800

    update fltf

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,13 +4,13 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./shpoly/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[20, 50, 20]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -19,9 +19,9 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.6 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.5 : 1.55}
+        position={isMobile ? [-1, -1, -2.2] : [0.5, -1.1, -2.5]}
+        rotation={[-0.01, -0.1, -0.07]}
       />
     </mesh>
   );
@@ -47,14 +47,14 @@ const ComputersCanvas = () => {
     <Canvas
       frameLoop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [10, 20, 10], fov: 15 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2.1}
+          minPolarAngle={Math.PI / 2.2}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
